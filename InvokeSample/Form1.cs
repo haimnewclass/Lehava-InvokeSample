da0ea52c61e8467ffc9c4a18c46af7523285df1f
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-05-11 20:46:10 +0300

    Add project files.

---
+++ InvokeSample/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvokeSample
+{
+    public partial class Form1 : Form
+    {
+        
+        RandomJob randomJob = new RandomJob();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            randomJob.sendToLabel = label1;
+            label1.Text = randomJob.num;
+                
+        }
+    }
+}
---
+++ InvokeSample/RandomJob.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvokeSample
+{
+    class RandomJob
+    {
+        public System.Windows.Forms.Label sendToLabel = null;
+        public RandomJob()
+        {
+            RunForRandomNumbers();
+        }
+
+        public string num;
+        Random r = new Random();
+        public string GetNum()
+        {            
+            return r.Next(1, 1000).ToString();
+        }
+
+        public void UpdateLabel()
+        {
+
+        }
+        public Task RunForRandomNumbers()
+        {
+            return Task.Factory.StartNew(() => {
+
+            while (true)
+            {
+                num = GetNum();
+                System.Threading.Thread.Sleep(1000);
+                // label
+                if (sendToLabel != null)
+                {
+                    //sendToLabel.Text = num;
+                    if (sendToLabel.InvokeRequired)
+                    {
+                            // invoke
+                            sendToLabel.Invoke(new Action(() => { sendToLabel.Text = num; }));
+                        }
+                        else
+                        {
+                            sendToLabel.Text = num;
+                        }
+
+                    }
+                }
+            });
+        }
+    }
+}
